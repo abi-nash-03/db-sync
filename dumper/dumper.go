b11e2a6b2d10b77c10ab0d23b6c33ad9721e979c
abi-nash-03 <rprabinash@gmail.com>
2026-03-26 21:57:47 +0530

    create mysql dump

--- dumper/dumper.go
+++ dumper/dumper.go
@@ -3,10 +3,40 @@ package dumper
 import (
 	"db-sync/config"
 	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
 )
 
 func Dump(c *config.Config) (string, error) {
 	fmt.Println("Dumping database...")
-    
-	return "", nil
+	timestamp := time.Now().Format("2006-01-02_15-04-05")
+	file_name := "dump_" + timestamp + ".sql"
+	path := "/tmp/" + file_name
+
+	// execute the mysqldump command
+	cmd := exec.Command(
+		"mysqldump",
+		"-u", c.Destination.User,
+		"-h", c.Destination.Host,
+		"-P", strconv.Itoa(c.Destination.Port),
+		c.Destination.Database)
+
+	// adding the password to env so that command wont prompt for password
+	cmd.Env = append(os.Environ(),
+		"MYSQL_PWD="+c.Destination.Password,
+	)
+
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	// write the output to a file
+	if err := os.WriteFile(path, out, 0644); err != nil {
+		return "", err
+	}
+
+	return path, nil
 }
--- main.go
+++ main.go
@@ -2,75 +2,89 @@ package main
 
 import (
 	// "db-sync/cmd"
-	"database/sql"
+	// "database/sql"
 	"db-sync/config"
-	"db-sync/tunnel"
+	"db-sync/dumper"
 	"fmt"
-	"log"
-	_ "github.com/go-sql-driver/mysql"
+	// "db-sync/tunnel"
+	// "fmt"
+	// "log"
+	// _ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	// cmd.Execute()
 
 	config.LoadConfig("config.yaml")
-
-	tunnel := &tunnel.Tunnel{
-		SSHHost:    config.AppConfig.Destination.SSH.Host,
-		SSHPort:    config.AppConfig.Destination.SSH.Port,
-		SSHUser:    config.AppConfig.Destination.SSH.User,
-		KeyPath:    config.AppConfig.Source.SSH.KeyPath,
-		RemotePort: config.AppConfig.Source.Port,
-		LocalPort:  0,
-	}
-
-	if err := tunnel.Open(); err != nil {
-		fmt.Println("Error opening tunnel:", err)
+	// =====================================
+	// Dumper testing
+	// =====================================
+	dump_path, err := dumper.Dump(config.AppConfig)
+	if err != nil {
+		fmt.Println("Error while generating dump %w", err)
 		return
 	}
+	fmt.Println("Dump generated at:", dump_path)
 
-	defer tunnel.Close()
-	fmt.Println("Tunnel opened on port:", tunnel.LocalPort)
+	// =====================================
+	// Tunneling testing
+	// =====================================
+	// tunnel := &tunnel.Tunnel{
+	// 	SSHHost:    config.AppConfig.Destination.SSH.Host,
+	// 	SSHPort:    config.AppConfig.Destination.SSH.Port,
+	// 	SSHUser:    config.AppConfig.Destination.SSH.User,
+	// 	KeyPath:    config.AppConfig.Source.SSH.KeyPath,
+	// 	RemotePort: config.AppConfig.Source.Port,
+	// 	LocalPort:  0,
+	// }
 
-	// --- 3. Connect MySQL through the tunnel ---
-	// DSN format: user:password@tcp(host:port)/dbname
-	dsn := fmt.Sprintf(
-		"%s:%s@tcp(127.0.0.1:%d)/%s?timeout=10s",
-		config.AppConfig.Destination.User,     // e.g. "root"
-		config.AppConfig.Destination.Password, // e.g. "secret"
-		tunnel.LocalPort,
-		config.AppConfig.Destination.Database, // e.g. "myapp" or leave blank as "information_schema" for testing
-	)
+	// if err := tunnel.Open(); err != nil {
+	// 	fmt.Println("Error opening tunnel:", err)
+	// 	return
+	// }
 
-	fmt.Println("DSN:", dsn)
+	// defer tunnel.Close()
+	// fmt.Println("Tunnel opened on port:", tunnel.LocalPort)
 
-	fmt.Println("Connecting to MySQL through tunnel...")
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatalf("failed to open db: %v", err)
-	}
-	defer db.Close()
+	// // --- 3. Connect MySQL through the tunnel ---
+	// // DSN format: user:password@tcp(host:port)/dbname
+	// dsn := fmt.Sprintf(
+	// 	"%s:%s@tcp(127.0.0.1:%d)/%s?timeout=10s",
+	// 	config.AppConfig.Destination.User,     // e.g. "root"
+	// 	config.AppConfig.Destination.Password, // e.g. "secret"
+	// 	tunnel.LocalPort,
+	// 	config.AppConfig.Destination.Database, // e.g. "myapp" or leave blank as "information_schema" for testing
+	// )
 
-	// --- 4. Run a simple test query ---
-	var version string
-	if err := db.QueryRow("SELECT VERSION()").Scan(&version); err != nil {
-		log.Fatalf("query failed: %v", err)
-	}
-	fmt.Printf("✓ MySQL is alive! Version: %s\n", version)
+	// fmt.Println("DSN:", dsn)
 
-	// --- 5. List databases (sanity check) ---
-	rows, err := db.Query("SHOW DATABASES")
-	if err != nil {
-		log.Fatalf("show databases failed: %v", err)
-	}
-	defer rows.Close()
+	// fmt.Println("Connecting to MySQL through tunnel...")
+	// db, err := sql.Open("mysql", dsn)
+	// if err != nil {
+	// 	log.Fatalf("failed to open db: %v", err)
+	// }
+	// defer db.Close()
 
-	fmt.Println("\nDatabases on remote server:")
-	for rows.Next() {
-		var name string
-		rows.Scan(&name)
-		fmt.Printf("  - %s\n", name)
-	}
+	// // --- 4. Run a simple test query ---
+	// var version string
+	// if err := db.QueryRow("SELECT VERSION()").Scan(&version); err != nil {
+	// 	log.Fatalf("query failed: %v", err)
+	// }
+	// fmt.Printf("✓ MySQL is alive! Version: %s\n", version)
 
-	fmt.Println("\n✓ All good! Tunnel + MySQL working correctly.")
+	// // --- 5. List databases (sanity check) ---
+	// rows, err := db.Query("SHOW DATABASES")
+	// if err != nil {
+	// 	log.Fatalf("show databases failed: %v", err)
+	// }
+	// defer rows.Close()
+
+	// fmt.Println("\nDatabases on remote server:")
+	// for rows.Next() {
+	// 	var name string
+	// 	rows.Scan(&name)
+	// 	fmt.Printf("  - %s\n", name)
+	// }
+
+	// fmt.Println("\n✓ All good! Tunnel + MySQL working correctly.")
 }
