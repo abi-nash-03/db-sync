539a2828bc380ed2c9bf164ce0de948b70ffc637
abi-nash-03 <rprabinash@gmail.com>
2026-03-31 23:11:39 +0530

    update time formats

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func main() {
 
 	startTime := time.Now()
 	config.LoadConfig("config.yaml")
-	fmt.Printf("Dump Started at %s\n", startTime.Format("2006-01-02 15:04:05"))
 
 	dumpPath, err := dumper.Dump(config.AppConfig)
 	if err != nil {
@@ -28,8 +27,8 @@ func main() {
 	endTime := time.Now()
 
 	fmt.Println("==============================================")
-	fmt.Printf("Start time: %s\n", startTime)
-	fmt.Printf("End time: %s\n", endTime)
+	fmt.Printf("Start time: %s\n", startTime.Format("2006-01-02 15:04:05"))
+	fmt.Printf("End time: %s\n", endTime.Format("2006-01-02 15:04:05"))
 	fmt.Printf("Total time Taken: %s\n", endTime.Sub(startTime))
 	fmt.Println("==============================================")
 
